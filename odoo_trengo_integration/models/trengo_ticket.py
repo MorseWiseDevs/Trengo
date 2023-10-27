9cef89bfa6b2c64dd50b9367e2a2c99292461844
MorseWiseDevs <126137704+MorseWiseDevs@users.noreply.github.com>
2023-10-27 14:27:43 +0400

    Add files via upload

--- odoo_trengo_integration/models/trengo_ticket.py
+++ odoo_trengo_integration/models/trengo_ticket.py
@@ -1,7 +1,4 @@
-import requests
-from odoo.exceptions import UserError
-
-from odoo import fields, models, api, _
+from odoo import fields, models, api
 
 
 class TrengoTicket(models.Model):
@@ -38,68 +35,26 @@ class TrengoTicket(models.Model):
             rec.is_helpdesk_button_visible = 'helpdesk.ticket' in rec.label_ids.object_to_create.mapped('model')
             rec.is_repair_button_visible = 'repair.order' in rec.label_ids.object_to_create.mapped('model')
 
-    # add other fields as necessary
-
-    def fetch_and_create_tickets_from_trengo(self):
-        self.env['trengo.label'].fetch_and_create_labels_from_trengo()
-        self.env['res.partner'].fetch_and_create_profiles_from_trengo()
-        url = self.env["ir.config_parameter"].sudo().get_param("account_contact_api.trengo_api_url")
-        api_key = self.env["ir.config_parameter"].sudo().get_param("account_contact_api.trengo_api_key")
-
-        if not (url and api_key):
-            raise UserError(_("Trengo API url or API key missing from system parameters"))
-
-        endpoint = f"{url}/tickets?status=CLOSED"
-
-        trengo_tickets = requests.get(endpoint, headers={"Authorization": f"Bearer {api_key}"})
-
-        for ticket in trengo_tickets.json()['data']:
-            if ticket['labels']:
-                existing_ticket = self.search([('trengo_id', '=', ticket['id'])], limit=1)
-                profiles = ticket.get('contact', {}).get('profile', {})
-                profile_id = False
-                for profile in profiles:
-                    profile_id = profile.get('id')
-                label_ids = []
-                contact_id = False
-                if profile_id:
-                    contact_id = self.env['res.partner'].search([('trengo_id', '=', profile_id)]).id
-                for label in ticket['labels']:
-                    label_ids.append(label['id'])
-                label_ids = list(set(label_ids))
-                if not existing_ticket:
-                    self.create({
-                        'trengo_id': str(ticket['id']),
-                        'label_ids': self.env['trengo.label'].search([('trengo_id', 'in', label_ids)]).ids,
-                        'contact_id': contact_id
-                    })
-                else:
-                    existing_ticket.write({
-                        'trengo_id': str(ticket['id']),
-                        'label_ids': self.env['trengo.label'].search([('trengo_id', 'in', label_ids)]).ids,
-                        'contact_id': contact_id
-                    })
-
     def action_create_helpdesk_ticket(self):
-        a = self.env['helpdesk.ticket'].create({
+        ticket = self.env['helpdesk.ticket'].create({
             'partner_id': self.contact_id.id,
             'name': self.trengo_id
         })
-        self.helpdesk_ticket_id = a
+        self.helpdesk_ticket_id = ticket
 
     def action_create_lead(self):
-        a = self.env['crm.lead'].create({
+        lead = self.env['crm.lead'].create({
             'partner_id': self.contact_id.id,
             'name': self.trengo_id
         })
-        self.lead_id = a
+        self.lead_id = lead
 
     def action_create_repair_order(self):
-        a = self.env['repair.order'].create({
+        order = self.env['repair.order'].create({
             'partner_id': self.contact_id.id,
             'product_id': 1
         })
-        self.repair_ticket_id = a
+        self.repair_ticket_id = order
 
     def action_view_help_desk_ticket(self):
         return {
